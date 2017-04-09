f38ee21834b850e0dd2a894dbcc0f4771ad930a7
Nekuake <ruizale55@gmail.com>
2017-04-09 16:45:32 +0200

    Update Test.bat

--- Test.bat
+++ Test.bat
@@ -6,9 +6,9 @@
 @For /f "tokens=3" %%* in (
    'route.exe print ^|findstr "\<0.0.0.0\>"'
    ) Do @Set "DefaultGateway=%%*"
-@echo Ping to Gateway ( "%DefaultGateway%") (No reply means no connection to the gateway)
+@echo Ping to Gateway ( "%DefaultGateway%") (No reply means no connection with the gateway)
 @ping %DefaultGateway% -n 1 | FIND "TTL="
-@echo Ping to Google (No reply means no connection to Google)
+@echo Ping to Google (No reply means no connection with Google)
 @ping google.es -n 1 | FIND "TTL="
 @echo Done
 @Pause
